f607c63023adc1fa9e978d922c5e3d3f2cf988ca
RHM <righthandman176@proton.me>
2025-10-06 20:16:23 +0200

    drop support for 1.14-1.15

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
 fix: players were unable to interact with blocks
+chore: drop support for 1.14-1.15
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ stonecutter {
     centralScript = "build.gradle.kts"
     kotlinController = true
     create(rootProject) {
-        versions("1.14.4", "1.15", "1.17.1", "1.18", "1.19", "1.20.1", "1.20.6", "1.21.1", "1.21.3", "1.21.5")
+        versions("1.17.1", "1.18", "1.19", "1.20.1", "1.20.6", "1.21.1", "1.21.3", "1.21.5")
         vcsVersion = "1.20.6"
         branch("fabric")
         branch("forge")
