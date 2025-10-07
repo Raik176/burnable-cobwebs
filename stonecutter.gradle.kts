32b82e8925ae1eb515f19752e3a925824f63e236
RHM <righthandman176@proton.me>
2025-10-08 00:22:14 +0200

    remove forced jackson version; updating is completly on hold until mod-publish-plugin doesn't use actually ancient dependencies

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -32,14 +32,6 @@ plugins {
 }
 stonecutter active "1.20.6" /* [SC] DO NOT EDIT */
 
-configurations.all {
-    resolutionStrategy {
-        force("com.fasterxml.jackson.core:jackson-databind:2.12.7.2")
-        force("com.fasterxml.jackson.core:jackson-core:2.12.7")
-        force("com.fasterxml.jackson.core:jackson-annotations:2.12.7")
-    }
-}
-
 val changelogProvider = layout.buildDirectory.file("CHANGELOG.md")
 changelogProvider.get().asFile.apply {
     if (!exists()) {
